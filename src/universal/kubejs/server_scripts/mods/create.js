a1b084c96bf6b666743e7e0ce916f149d05878e0
Moritz Ruth <dev@moritzruth.de>
2022-09-01 15:42:07 +0200

    Fix crushed recipes

--- src/universal/kubejs/server_scripts/mods/create.js
+++ src/universal/kubejs/server_scripts/mods/create.js
@@ -28,6 +28,9 @@ onEvent("recipes", event => {
     })
 
     ;["iron", "gold", "copper", "zinc"].forEach(ore => {
+        event.remove({ id: `create:crushing/raw_${ore}`})
+        event.remove({ id: `create:crushing/raw_${ore}_block`})
+
         event.custom({
             type: "create:crushing",
             processingTime: 2 * 20,
@@ -43,6 +46,27 @@ onEvent("recipes", event => {
                 },
                 {
                     item: `create:crushed_${ore}_ore`,
+                    chance: 0.25
+                }
+            ]
+        })
+
+        event.custom({
+            type: "create:crushing",
+            processingTime: 2 * 20,
+            ingredients: [
+                {
+                    tag: `c:storage_blocks/raw_${ore}`
+                }
+            ],
+            results: [
+                {
+                    item: `create:crushed_${ore}_ore`,
+                    count: 2 * 9
+                },
+                {
+                    item: `create:crushed_${ore}_ore`,
+                    count: 4,
                     chance: 0.25
                 }
             ]
